2d6f07b3bd2fd85c97baf72fd15bbb696607f68d
Taiwoshakirah <shakirattaiwo6@gmail.com>
2024-04-22 13:14:25 -0700

    first commit

---
+++ routes/taskRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { allTasks, singleTask, createTask, updateTask, deleteTask } = require('../controllers/taskController');
+const methodNotAllowed = require('../utils/methodNotAllowed');
+const auth = require('../middlewares/auth');
+const router = express.Router()
+
+
+router.route('/').get(auth, allTasks).post(auth, createTask).all(methodNotAllowed)
+
+  router.route('/:id').get(auth, singleTask).patch(auth, updateTask).delete(auth, deleteTask).all(methodNotAllowed)
+
+
+
+  module.exports = router
